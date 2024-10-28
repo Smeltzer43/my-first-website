8219f652538c727d366b877147222eb18eec0bfd
Chad Smeltzer <77508664+Smeltzer43@users.noreply.github.com>
2024-10-28 13:54:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-function sayHello() {
-    alert("Hello, GitHub Traveler!");
-}
+document.querySelector("#contact a").addEventListener("click", function() {
+    alert("Feel free to reach out to me via email!");
+});
